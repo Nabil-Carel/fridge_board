2e623fce9a47b1f5dd4c7baaa93ce38256d76ce7
Nabil Gbaguidi <nabilgbaguidi8@gmail.com>
2021-10-03 20:17:41 -0400

    set timezone fixed to gmt -4

--- src/App.js
+++ src/App.js
@@ -38,6 +38,23 @@ function App() {
     forecastIconColor: "#fff",
   };
 
+  function calcTime(offset) {
+    // create Date object for current location
+    var d = new Date();
+
+    // convert to msec
+    // subtract local time zone offset
+    // get UTC time in msec
+    var utc = d.getTime() + d.getTimezoneOffset() * 60000;
+
+    // create new Date object for different city
+    // using supplied offset
+    var nd = new Date(utc + 3600000 * offset);
+
+    // return time as a string
+    return nd.toLocaleString();
+  }
+
   useEffect(() => {
     async function fetchTableData() {
       const promises = [getTasks(), getRoommates()];
@@ -102,7 +119,7 @@ function App() {
           tableLayout="fixed"
           size="small"
         />
-        <p>{`Last update: ${date.toLocaleTimeString()}`} </p>
+        <p>{`${calcTime(-4)}`}</p>
       </div>
     </div>
   );
